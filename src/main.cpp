571b900a1611410f3838acdac335dbf24a366fa7
Clay DeGruchy <clay.degruchy+github@gmail.com>
2024-04-29 16:00:15 +0200

    really just fucked up

--- src/main.cpp
+++ src/main.cpp
@@ -1,226 +1,64 @@
-#include "esp_camera.h"
+
 #include "secrets.h"
 #include <Arduino.h>
-#include <WebServer.h>
-#include <WiFi.h>
 
-#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+#include <eloquent_esp32cam.h>
+#include <eloquent_esp32cam/viz/mjpeg.h>
 
-#if defined(CAMERA_MODEL_ESP_EYE)
-#define PWDN_GPIO_NUM -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 4
-#define SIOD_GPIO_NUM 18
-#define SIOC_GPIO_NUM 23
-
-#define Y9_GPIO_NUM 36
-#define Y8_GPIO_NUM 37
-#define Y7_GPIO_NUM 38
-#define Y6_GPIO_NUM 39
-#define Y5_GPIO_NUM 35
-#define Y4_GPIO_NUM 14
-#define Y3_GPIO_NUM 13
-#define Y2_GPIO_NUM 34
-#define VSYNC_GPIO_NUM 5
-#define HREF_GPIO_NUM 27
-#define PCLK_GPIO_NUM 25
-
-#elif defined(CAMERA_MODEL_XIAO_ESP32S3)
-#define PWDN_GPIO_NUM -1
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 10
-#define SIOD_GPIO_NUM 40
-#define SIOC_GPIO_NUM 39
-
-#define Y9_GPIO_NUM 48
-#define Y8_GPIO_NUM 11
-#define Y7_GPIO_NUM 12
-#define Y6_GPIO_NUM 14
-#define Y5_GPIO_NUM 16
-#define Y4_GPIO_NUM 18
-#define Y3_GPIO_NUM 17
-#define Y2_GPIO_NUM 15
-#define VSYNC_GPIO_NUM 38
-#define HREF_GPIO_NUM 47
-#define PCLK_GPIO_NUM 13
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
-
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
-
-#else
-#error "Camera model not selected"
-#endif
-
-/* Constant defines -------------------------------------------------------- */
-#define EI_CAMERA_RAW_FRAME_BUFFER_COLS 320
-#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS 240
-#define EI_CAMERA_FRAME_BYTE_SIZE 3
-
-/* Private variables ------------------------------------------------------- */
-static camera_config_t camera_config = {
-    .pin_pwdn = PWDN_GPIO_NUM,
-    .pin_reset = RESET_GPIO_NUM,
-    .pin_xclk = XCLK_GPIO_NUM,
-    .pin_sscb_sda = SIOD_GPIO_NUM,
-    .pin_sscb_scl = SIOC_GPIO_NUM,
-
-    .pin_d7 = Y9_GPIO_NUM,
-    .pin_d6 = Y8_GPIO_NUM,
-    .pin_d5 = Y7_GPIO_NUM,
-    .pin_d4 = Y6_GPIO_NUM,
-    .pin_d3 = Y5_GPIO_NUM,
-    .pin_d2 = Y4_GPIO_NUM,
-    .pin_d1 = Y3_GPIO_NUM,
-    .pin_d0 = Y2_GPIO_NUM,
-    .pin_vsync = VSYNC_GPIO_NUM,
-    .pin_href = HREF_GPIO_NUM,
-    .pin_pclk = PCLK_GPIO_NUM,
-
-    // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-    .xclk_freq_hz = 20000000,
-    .ledc_timer = LEDC_TIMER_0,
-    .ledc_channel = LEDC_CHANNEL_0,
-
-    .pixel_format = PIXFORMAT_JPEG, // YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size =
-        FRAMESIZE_QVGA, // QQVGA-UXGA Do not use sizes above QVGA when not JPEG
-
-    .jpeg_quality = 12, // 0-63 lower number means higher quality
-    .fb_count =
-        1, // if more than one, i2s runs in continuous mode. Use only with JPEG
-    .fb_location = CAMERA_FB_IN_PSRAM,
-    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
-};
-
-// const char *ssid = WIFI_SSID
-// const char *password = WIFI_PASSWORD
-
-const int captureInterval = 2000; // Capture an image every 5 seconds
-unsigned long lastCaptureTime = 0;
-
-WebServer server(80);
-// Set your Static IP address
-// IPAddress local_IP(192, 168, 1, 25);
+using namespace eloq;
+using namespace eloq::viz;
 
 //
-// ESP32 static IP
-IPAddress staticIP(192, 168, 1, 25);
-// IP Address of your network gateway (router)
-IPAddress gateway(192, 168, 1, 1);
-// Subnet mask
-IPAddress subnet(255, 255, 255, 0);
-
-void handleRequest() {
-  // Serve the captured image
-  camera_fb_t *fb = esp_camera_fb_get();
-  if (!fb) {
-    server.send(500, "text/plain", "Failed to capture image");
-    return;
-  }
-
-  server.setContentLength(fb->len);
-  server.sendHeader("Content-Disposition", "inline; filename=captured.jpg");
-  server.send(200, "image/jpeg");
-  server.sendContent((const char *)fb->buf, fb->len);
-
-  esp_camera_fb_return(fb);
-}
-
-void captureImage() {
-  camera_fb_t *fb = NULL;
-  fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Camera capture failed");
-    return;
-  }
-
-  // You can save or process the image here, or simply discard it for now
-
-  esp_camera_fb_return(fb);
-}
+// // ESP32 static IP
+// IPAddress staticIP(192, 168, 1, 25);
+// // IP Address of your network gateway (router)
+// IPAddress gateway(192, 168, 1, 1);
+// // Subnet mask
+// IPAddress subnet(255, 255, 255, 0);
 
 void setup() {
-  Serial.begin(115200); // Debug only
+  Serial.begin(115200);
+  Serial.println("___MJPEG STREAM SERVER___");
 
-  while (!Serial)
-    ;
-  Serial.println("Edge Impulse Inferencing Demo");
-  // if (ei_camera_init() == false) {
-  //   Serial.println("Failed to initialize Camera!\r\n");
-  // } else {
-  //   Serial.println("Camera initialized\r\n");
-  // }
+  // camera settings
+  // replace with your own model!
 
-  // MQTT
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  Serial.println("camera.pinout.xiao();");
+  camera.pinout.xiao();
+  Serial.println("camera.pinout.aithinker();");
+  camera.pinout.aithinker();
+  Serial.println("camera.brownout.disable();");
+  camera.brownout.disable();
+  Serial.println("camera.resolution.qvga();");
+  camera.resolution.qvga();
+  Serial.println("camera.quality.low();");
+  camera.quality.low();
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  // // init camera
+  while (!camera.begin().isOk())
+    Serial.println(camera.exception.toString());
+  Serial.println("Camera OK");
 
-  Serial.println("Connected..!");
+  // // connect to WiFi
+  // while (!wifi.connect().isOk())
+  //   Serial.println(wifi.exception.toString());
+  // Serial.println("WiFi OK");
 
-  Serial.print("Current ESP32 IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Gateway (router) IP: ");
-  Serial.println(WiFi.gatewayIP());
-  Serial.print("Subnet Mask: ");
-  Serial.println(WiFi.subnetMask());
-  Serial.print("Primary DNS: ");
-  Serial.println(WiFi.dnsIP(0));
-  Serial.print("Secondary DNS: ");
-  Serial.println(WiFi.dnsIP(1));
+  // // start mjpeg http server
+  // while (!mjpeg.begin().isOk())
+  //   Serial.println(mjpeg.exception.toString());
+  // Serial.println("MjpegStream OK");
 
-  esp_err_t err = esp_camera_init(&camera_config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  } else {
-    Serial.printf("Camera initialized\r\n");
-  }
-
-  //  Start the web server
-  server.on("/", HTTP_GET, handleRequest);
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-void flashLED() {
-  Serial.println("Flashing LED");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
+  // Serial.println(mjpeg.address());
 }
 
 int i = 0;
+
 void loop() {
-
-  // put your main code here, to run repeatedly:
-  // delay(3000);
-  server.handleClient();
-
-  // Capture an image every 5 seconds
-  if (millis() - lastCaptureTime > captureInterval) {
-    captureImage();
-    lastCaptureTime = millis();
-    flashLED();
+  if (i % 12345 == 0) {
+    Serial.print("loop: ");
+    Serial.println(i);
   }
+
+  i++;
 }
